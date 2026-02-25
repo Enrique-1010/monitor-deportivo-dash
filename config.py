fc98855366a9b593ef428e6757952fbeefd2b0e4
Enrique-1010 <151920036+Enrique-1010@users.noreply.github.com>
2026-02-25 12:52:30 +0100

    Add configuration constants for the application

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration Constants for the Application
+
+DATABASE_PATH = '/path/to/database'
+SECURITY_THRESHOLDS = {
+    'login_attempts': 5,
+    'password_length': 8,
+}
+APPLICATION_SETTINGS = {
+    'debug_mode': True,
+    'allowed_hosts': ['localhost', 'example.com'],
+}
